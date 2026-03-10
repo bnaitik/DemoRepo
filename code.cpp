f424aad0103578b285fb290406d687e7b85b142b
Naitik Bansal <bansalnaitik66@gmail.com>
2026-03-10 21:35:12 +0530

    Code added in cpp file && Executable file made

---
+++ code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int sum;
+
+    cout<<"enter a : ";
+    cin>>a;
+    cout<<"enter b : ";
+    cin>>b;
+    cout<<"sum is : "<<a+b;
+    cout<<"";
+
+    return 0;
+}
